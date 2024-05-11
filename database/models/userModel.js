a46ca665a2799de1d9c3c93dc39740f3f9f60e19
Ghayoor Hussain <ghayoorhussain701@gmail.com>
2024-05-11 19:47:06 +0500

    create user-model

---
+++ database/models/userModel.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+    toObject: {
+      transform: function (doc, ret, options) {
+        ret.id = ret._id;
+        delete ret._id;
+        delete ret.password;
+        delete ret.__v;
+        return ret;
+      },
+    },
+  }
+);
+
+module.exports = mongoose.model("User", userSchema);
